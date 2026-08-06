2926a2a0eb79114dbfa75011f274b5222e51acde
mathias <mathias.paulenko@gmail.com>
2026-08-06 08:39:26 +0800

    release: v1.0.1

--- behave_modern_sheets_report/__init__.py
+++ behave_modern_sheets_report/__init__.py
@@ -25,7 +25,7 @@ from .ods_writer import ODSWriter
 from .xlsx_formatter import XLSXFormatter
 from .xlsx_writer import XLSXWriter
 
-__version__ = "0.1.0"
+__version__ = "1.0.1"
 
 __all__ = [
     "BaseSheetsFormatter",
